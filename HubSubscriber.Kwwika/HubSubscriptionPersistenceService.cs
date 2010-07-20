cc7d81e50bd2959b9afc1847ceaf0bfd8138b662
Kwwika <phil@kwwika.com>
2010-07-20 21:12:56 +0100

    Locking access to the DB to stop multiple thread access.
    Removing build files.

--- HubSubscriber.Kwwika/HubSubscriptionPersistenceService.cs
+++ HubSubscriber.Kwwika/HubSubscriptionPersistenceService.cs
@@ -8,10 +8,12 @@ using Kwwika.Common.Logging;
 
 namespace HubSubscriber.Kwwika
 {
-    public class HubSubscriptionPersistenceService: IHubSubscriptionPersistenceService
+    public class HubSubscriptionPersistenceService : IHubSubscriptionPersistenceService
     {
+        private readonly object _dbAccessLock = new object();
         private ILoggingService _loggingService;
         private HubSubscriberEntities _entities;
+
         public HubSubscriptionPersistenceService(ILoggingService loggingService)
         {
             _loggingService = loggingService;
@@ -22,82 +24,101 @@ namespace HubSubscriber.Kwwika
 
         public void StoreSubscription(SubscriptionModel model)
         {
-            Subscription newSubscription = _entities.CreateObject<Subscription>();
-            newSubscription.Callback = model.Callback;
-            newSubscription.Digest = model.Digest;
-            newSubscription.Mode = model.Mode;
-            newSubscription.Verify = model.Verify;
-            newSubscription.Topic = model.Topic;
-            newSubscription.LastUpdated = DateTime.Now;
+            lock (_dbAccessLock)
+            {
+                Subscription newSubscription = _entities.CreateObject<Subscription>();
+                newSubscription.Callback = model.Callback;
+                newSubscription.Digest = model.Digest;
+                newSubscription.Mode = model.Mode;
+                newSubscription.Verify = model.Verify;
+                newSubscription.Topic = model.Topic;
+                newSubscription.LastUpdated = DateTime.Now;
 
-            _loggingService.Info("Saving model to the database " + model);
+                _loggingService.Info("Saving model to the database " + model);
 
-            _entities.AddToSubscriptionsSet(newSubscription);
-            _entities.SaveChanges();
+                _entities.AddToSubscriptionsSet(newSubscription);
+                _entities.SaveChanges();
 
-            model.Id = newSubscription.Id;
+                model.Id = newSubscription.Id;
+            }
         }
 
         public IEnumerable<SubscriptionModel> GetSubscriptionsList()
         {
             IList<SubscriptionModel> subscriptionModels = new List<SubscriptionModel>();
-            foreach (Subscription sub in _entities.SubscriptionsSet.ToList())
+            lock (_dbAccessLock)
             {
-                subscriptionModels.Add(CreateSubscriptionModelFromSubscription(sub));
+                foreach (Subscription sub in _entities.SubscriptionsSet.ToList())
+                {
+                    subscriptionModels.Add(CreateSubscriptionModelFromSubscription(sub));
+                }
             }
             return subscriptionModels;
         }
 
         public void DeleteSubscriptionById(int id)
         {
-            Subscription sub = _entities.SubscriptionsSet.First(m => m.Id == id);
-            _entities.SubscriptionsSet.DeleteObject(sub);
-            _entities.SaveChanges();
+            lock (_dbAccessLock)
+            {
+                Subscription sub = _entities.SubscriptionsSet.First(m => m.Id == id);
+                _entities.SubscriptionsSet.DeleteObject(sub);
+                _entities.SaveChanges();
+            }
         }
 
         public int GetSubscriptionCountById(int id)
         {
-            return _entities.SubscriptionsSet.Count<Subscription>(m => m.Id == id);
+            lock (_dbAccessLock)
+            {
+                int count = _entities.SubscriptionsSet.Count<Subscription>(m => m.Id == id);
+                return count;
+            }
         }
 
         public SubscriptionModel GetSubscriptionById(int id)
         {
-            Subscription sub = _entities.SubscriptionsSet.First(m => m.Id == id);
-            SubscriptionModel model = CreateSubscriptionModelFromSubscription(sub);
-            return model;
+            lock (_dbAccessLock)
+            {
+                Subscription sub = _entities.SubscriptionsSet.First(m => m.Id == id);
+                SubscriptionModel model = CreateSubscriptionModelFromSubscription(sub);
+                return model;
+            }
         }
 
         public void SaveChanges(SubscriptionModel model)
         {
-            Subscription sub = _entities.SubscriptionsSet.First(m => m.Id == model.Id);
+            lock (_dbAccessLock)
+            {
+                Subscription sub = _entities.SubscriptionsSet.First(m => m.Id == model.Id);
 
-            sub.Callback = model.Callback;
-            sub.Digest = model.Digest;
-            sub.LastUpdated = model.LastUpdated;
-            sub.Mode = model.Mode;
-            sub.PendingDeletion = model.PendingDeletion;
-            sub.Topic = model.Topic;
-            sub.Verified = model.Verified;
-            sub.Verify = model.Verify;
+                sub.Callback = model.Callback;
+                sub.Digest = model.Digest;
+                sub.LastUpdated = model.LastUpdated;
+                sub.Mode = model.Mode;
+                sub.PendingDeletion = model.PendingDeletion;
+                sub.Topic = model.Topic;
+                sub.Verified = model.Verified;
+                sub.Verify = model.Verify;
 
-            _entities.SaveChanges();
+                _entities.SaveChanges();
+            }
         }
         #endregion
 
         private SubscriptionModel CreateSubscriptionModelFromSubscription(Subscription sub)
         {
             var model = new SubscriptionModel()
-                    {
-                        Id = sub.Id,
-                        Callback = sub.Callback,
-                        Mode = sub.Mode,
-                        Digest = sub.Digest,
-                        Topic = sub.Topic,
-                        Verify = sub.Verify,
-                        PendingDeletion = sub.PendingDeletion,
-                        Verified = sub.Verified,
-                        LastUpdated = sub.LastUpdated
-                    };
+            {
+                Id = sub.Id,
+                Callback = sub.Callback,
+                Mode = sub.Mode,
+                Digest = sub.Digest,
+                Topic = sub.Topic,
+                Verify = sub.Verify,
+                PendingDeletion = sub.PendingDeletion,
+                Verified = sub.Verified,
+                LastUpdated = sub.LastUpdated
+            };
 
             return model;
         }
